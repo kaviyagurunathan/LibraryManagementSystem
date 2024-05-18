a5ffecfed8e495a7d2dd1c7a52d5bc7ddbbd5575
kaviyagurunathan <132463988+kaviyagurunathan@users.noreply.github.com>
2024-05-18 07:22:27 +0530

    Add files via upload

---
+++ output.java
@@ -0,0 +1,95 @@
+"C:\Program Files\Java\jdk-16.0.2\bin\java.exe" "-javaagent:C:\Users\WELCOME\AppData\Local\JetBrains\IntelliJ IDEA Community Edition 2024.1.1\lib\idea_rt.jar=62942:C:\Users\WELCOME\AppData\Local\JetBrains\IntelliJ IDEA Community Edition 2024.1.1\bin" -Dfile.encoding=UTF-8 -classpath C:\Users\WELCOME\IdeaProjects\LMS\out\production\LMS Main
+Welcome to Book Management Application
+1.Add Book
+2.Show All Books
+3.Show Available Books
+4.Borrow Book
+5.Return Book
+6.Exit
+
+Enter Your Choice !
+1
+Enter Book ID 
+0001
+Enter Author
+Leo Talstoy
+Enter Title
+WAR AND PEACE
+Enter Publish Year of Book 
+1939
+Book Added Successfully !!!
+Welcome to Book Management Application
+1.Add Book
+2.Show All Books
+3.Show Available Books
+4.Borrow Book
+5.Return Book
+6.Exit
+
+Enter Your Choice !
+2
+
+----------------------------------------------------------------------------------------------
+ID          TITLE         AUTHOR   PUBLISH YEAR     STATUS
+----------------------------------------------------------------------------------------------
+0001    Leo Talstoy  WAR AND PEACE           1939      Available
+
+----------------------------------------------------------------------------------------------
+Welcome to Book Management Application
+1.Add Book
+2.Show All Books
+3.Show Available Books
+4.Borrow Book
+5.Return Book
+6.Exit
+
+Enter Your Choice !
+3
+
+----------------------------------------------------------------------------------------------
+ID          TITLE         AUTHOR   PUBLISH YEAR     STATUS
+----------------------------------------------------------------------------------------------
+0001    Leo Talstoy  WAR AND PEACE           1939      Available
+
+----------------------------------------------------------------------------------------------
+Welcome to Book Management Application
+1.Add Book
+2.Show All Books
+3.Show Available Books
+4.Borrow Book
+5.Return Book
+6.Exit
+
+Enter Your Choice !
+4
+Enter Book ID 
+0001
+Book Borrowed Successfully !!!
+Borrowed Book Details: Book{id='0001', title='Leo Talstoy', author='WAR AND PEACE', publishYear='1939', status='Not Available'}
+Welcome to Book Management Application
+1.Add Book
+2.Show All Books
+3.Show Available Books
+4.Borrow Book
+5.Return Book
+6.Exit
+
+Enter Your Choice !
+5
+Enter Book ID 
+0001
+Book Returned Successfully !!!
+Returned Book Details: Book{id='0001', title='Leo Talstoy', author='WAR AND PEACE', publishYear='1939', status='Available'}
+Welcome to Book Management Application
+1.Add Book
+2.Show All Books
+3.Show Available Books
+4.Borrow Book
+5.Return Book
+6.Exit
+
+Enter Your Choice !
+6
+Thank you for Using Application !!
+
+Process finished with exit code 0
